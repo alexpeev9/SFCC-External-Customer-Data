146cac45f054769af8260c4f60e2347956726fe3
Aleksandar Peev <alexpeev9@abv.bg>
2023-03-02 18:17:14 +0200

    Implement PUT logic in firebase

---
+++ cartridges/int_external_data/cartridge/controllers/Address.js
@@ -0,0 +1,60 @@
+"use strict";
+
+/**
+ * @namespace Address
+ */
+
+var server = require("server");
+
+var URLUtils = require("dw/web/URLUtils");
+var Resource = require("dw/web/Resource");
+var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
+var userLoggedIn = require("*/cartridge/scripts/middleware/userLoggedIn");
+var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
+
+/**
+ * Address-SaveAddress : Save a new or existing address
+ * @name Base/Address-SaveAddress
+ * @function
+ * @memberof Address
+ * @param {middleware} - csrfProtection.validateAjaxRequest
+ * @param {querystringparameter} - addressId - a string used to identify the address record
+ * @param {httpparameter} - dwfrm_address_addressId - An existing address id (unless new record)
+ * @param {httpparameter} - dwfrm_address_firstName - A person’s first name
+ * @param {httpparameter} - dwfrm_address_lastName - A person’s last name
+ * @param {httpparameter} - dwfrm_address_address1 - A person’s street name
+ * @param {httpparameter} - dwfrm_address_address2 -  A person’s apartment number
+ * @param {httpparameter} - dwfrm_address_country - A person’s country
+ * @param {httpparameter} - dwfrm_address_states_stateCode - A person’s state
+ * @param {httpparameter} - dwfrm_address_city - A person’s city
+ * @param {httpparameter} - dwfrm_address_postalCode - A person’s united states postel code
+ * @param {httpparameter} - dwfrm_address_phone - A person’s phone number
+ * @param {httpparameter} - csrf_token - hidden input field CSRF token
+ * @param {category} - sensitive
+ * @param {returns} - json
+ * @param {serverfunction} - post
+ */
+server.append(
+    "SaveAddress",
+    csrfProtection.validateAjaxRequest,
+    function (req, res, next) {
+        var CustomerMgr = require("dw/customer/CustomerMgr");
+        var Transaction = require("dw/system/Transaction");
+        var formErrors = require("*/cartridge/scripts/formErrors");
+        var accountHelpers = require("*/cartridge/scripts/helpers/accountHelpers");
+        var addressHelpers = require("*/cartridge/scripts/helpers/addressHelpers");
+
+        var addressForm = server.forms.getForm("address");
+        var addressFormObj = addressForm.toObject();
+        addressFormObj.addressForm = addressForm;
+        var customer = CustomerMgr.getCustomerByCustomerNumber(
+            req.currentCustomer.profile.customerNo
+        );
+
+        const response = userService
+            .execute()
+            .call({ method: "PUT", route: "/users.json", body }).object;
+
+        return next();
+    }
+);
